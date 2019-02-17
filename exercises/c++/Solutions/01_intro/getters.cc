d5747b4402772ef1cc885882be274d9af543a5a3
alessiapaoletti <paoletti.alessia@gmail.com>
2019-02-17 18:18:00 +0100

    fixed

--- exercises/c++/Solutions/01_intro/getters.cc
+++ exercises/c++/Solutions/01_intro/getters.cc
@@ -4,27 +4,40 @@
 #include <string>
 
 
-int get_int(){
+void get_int(){
 	double a;
-	std::cout << "Insert an integer number: " << '\n';
+	std::cout << "Insert an integer number: " << std::endl;
 	while (std::cin>>a){
-		if (a==floor(a))  break;
+		if (a==floor(a))  {
+			std::cout << "This is an integer " << std::endl;
+			break;
+		} else {
+			std::cin.clear();
+			std::cin.ignore();
+			std::cout << "It is not an integer " << std::endl;
+		}
+	}
+}
+
+void get_double(){
+std::cout << "Insert a double number:\n";
+double a;
+while(std::cin>>a){
+	if(a!=(int)a){
+		std::cout<<"This is a double!"<<std::endl;
+		break;
+  }	else{
 		std::cin.clear();
 		std::cin.ignore();
+		std::cout<<"This is not a double "<<std::endl;
+
+	    }
 	}
-	return a;
 }
-//
-//
-// double get_double(){
-//
-//
-// }
 
 int main () {
-	int resultInt = get_int();
-	std::cout << "The integer number is: " << resultInt<< '\n';
-	//double resultDouble = get_double();
+	get_int();
+	get_double();
 
 	return 0;
 
